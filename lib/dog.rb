75e390b4ebaaecbbea619c92d383d750cecec9a4
Sydney Hunter <sydney.jane.hunter@gmail.com>
2019-11-24 23:01:55 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,19 @@ class Dog
     @breed = breed
   end
  
-  def breed
+  def breed 
     @breed
+  end
+    
+  def initialize(name)
+    @name = name
+  end
+ 
+  def name=(name)
+    @name = name
+  end
+ 
+  def name
+    @name
   end
 end
